acde55fec1c9f57f768b5663bedc0eba855cb703
Nadia <nadiahosisy@gmail.com>
2023-09-19 20:49:06 +0300

    add

--- src/contexts/Context.jsx
+++ src/contexts/Context.jsx
@@ -1,3 +1,39 @@
 import { createContext, useState, useContext } from "react";
 
 const AppContext = createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSideBar = () => {
+    setIsSidebarOpen(true);
+  };
+  const closeSideBar = () => {
+    setIsSidebarOpen(false);
+  };
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isModalOpen,
+        openModal,
+        openSideBar,
+        closeModal,
+        closeSideBar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
